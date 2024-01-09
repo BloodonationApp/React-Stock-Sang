bc4d93ba5f302fc4b3dfabe1445474fccd60bf9b
BloodonationApp <yassinebitw@gmail.com>
2024-01-09 02:37:32 +0100

    Last commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import AdminProfile from "./adminProfile";
+import AdminLogin from "./adminLogin";
+import Stock from "./Stock";
+import RendezVous from "./RendezVous";
+import AjoutBanque from "./ajoutBanque";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/adminProfile" element={<AdminProfile/>} />
+                <Route path="/adminLogin" element={<AdminLogin/>} />
+                <Route path="/stock" element={<Stock/>} />
+                <Route path="/rendezvous" element={<RendezVous/>} />
+                <Route path="/ajoutBanque" element={<AjoutBanque/>} />
+            </Routes>
+        </BrowserRouter>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
